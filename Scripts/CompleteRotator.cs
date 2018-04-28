e6ca4d7f1525bb60dcac10bc0b28a60ac61d8156
Steven Kha <stevenvkha@gmail.com>
2018-04-27 22:17:28 -0700

    upload please

---
+++ Scripts/CompleteRotator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CompleteRotator : MonoBehaviour {
+
+
+    //removed the void start because we don't need to initialize 
+    //any physical force later called in void Update
+
+	//Update is called every frame
+	void Update () 
+	{
+		//Rotate thet transform of the game object this is attached to by 45 degrees, taking into account the time elapsed since last frame.
+        //rotating 45 degrees around the world's Z's axis 
+        //https://docs.unity3d.com/ScriptReference/Transform.Rotate.html
+		transform.Rotate (new Vector3 (0, 0, 45) * Time.deltaTime);
+        //only using the eulers or rotations needed to affect the pickups
+        
+	}
+}
